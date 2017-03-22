71e7745867fbc8397dbf023d81fcccee4402b312
ryotax <r_kotaka@ubicast.com>
2017-03-22 13:37:34 +0900

    終了判定ロジックを修正。(指定回数読み込んだらではなく、読込結果が3回空だったらにした。)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,12 +12,12 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("start")
     val messages = args(0).toInt
-    (new Thread {override def run(): Unit = {receiveMessage(messages)}}).start()
+    (new Thread {override def run(): Unit = {receiveMessage()}}).start()
     Thread.sleep(3000)
     sendMessages(messages)
   }
 
-  def receiveMessage(n: Int): Unit = {
+  def receiveMessage(): Unit = {
     val sqsAsync  = new AmazonSQSAsyncClient()
     val bufferedSqs = new AmazonSQSBufferedAsyncClient(sqsAsync)
 
@@ -25,16 +25,22 @@ object Main {
             .withMaxNumberOfMessages(1)
             .withQueueUrl(SqsUrl)
 
-    var receives = 0
-    while (receives < n) {
+    var message = 0
+    var noMessage = 0
+    while (noMessage < 3) {
       val rx: ReceiveMessageResult = bufferedSqs.receiveMessage(receiveRq)
-      for (msg <- rx.getMessages.asScala) {
-        consumeMessage(msg)
-        receives += 1
-        println(receives)
-        bufferedSqs.deleteMessageAsync(new DeleteMessageRequest(SqsUrl, msg.getReceiptHandle))
+      val messageBuffer = rx.getMessages.asScala
+      if (messageBuffer.isEmpty) {
+        noMessage += 1
+      } else {
+        for (msg <- messageBuffer) {
+          consumeMessage(msg)
+          message += 1
+          println(s"n = ${message}")
+          bufferedSqs.deleteMessageAsync(new DeleteMessageRequest(SqsUrl, msg.getReceiptHandle))
+        }
       }
-      println("receive loop")
+      println("loop")
     }
     println("received")
     System.exit(0);
@@ -42,13 +48,13 @@ object Main {
 
   private def consumeMessage(msg: Message) {
     println(s"process ${msg.getBody}, ${msg.getMessageId}")
-    Thread.sleep(100)
+    Thread.sleep(10)
   }
 
   def sendMessages(n: Int): Unit = {
     println("send messages")
     val sqsAsync  = new AmazonSQSAsyncClient()
-    (0 to n) foreach {i =>
+    (0 until n) foreach {i =>
       val req = new SendMessageRequest(SqsUrl, s"msg: ${i}")
       sqsAsync.sendMessageAsync(req)
     }
